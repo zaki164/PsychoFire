1b9d0ebc80993c3d5261529921b0f3a0184a8b98
zaki <92805900+zaki164@users.noreply.github.com>
2023-01-23 21:11:00 +0200

    console commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import Layout from './Layout/Layout';
 import './App.css';
+console.log('zaki')
+console.log('medo')
 
 const App = () => (
   <Router>
